6ebfa067e25e6b13cb37d7d572c40686a3441275
Sap-Tea <marcosbarroso.info@gmail.com>
2025-10-23 16:43:47 -0300

    subindo alteracao

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,9 +9,12 @@ class FornecedorController extends Controller
     public function index(){
 
         $fornecedores = [
-    
-    
-    
+            [ "nome"=> "Marcos barroso",
+              "status" => "S"],
+           [ "nome"=> "Eliane Barroso",
+              "status" => "N"],
+                      [ "nome"=> "Carlos barroso",
+                        "status" => "N"]
     
     ];
         return view('app.Fornecedor.index',compact('fornecedores'));
--- resources/views/app/Fornecedor/index.blade.php
+++ resources/views/app/Fornecedor/index.blade.php
@@ -1,24 +1,26 @@
 <h3>Testando Blade</h3>
-{{-- comentarios--}}
-{{'xxxxxxxxxxxxxxxxx'}}
-<br/>
-<?='yyyyyyyyyyyyyyyyyy'?>
-<br/>
-
 
 @php
-//@dd($fornecedores)
-@endphp
-
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-
-@elseif(count($fornecedores) >= 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-
-@else
-    <h3>Não existem fornecedores cadastrados</h3>
-@endif
-
+    
 
 @endphp
+
+@php
+    //d ($fornecedores);
+@endphp
+Fornecedor : {{$fornecedores[1]["nome"]}};
+<br>
+Status     :{{$fornecedores[1]["status"]}};
+<br>
+@if($fornecedores[0]['status']== 'N')
+    fornecedore inativo
+@endif   
+
+
+
+
+
+
+
+
+
